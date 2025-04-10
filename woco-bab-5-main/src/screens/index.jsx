8123e81e0aa914d800a70990bfd6671f4df07a3c
Nama Kamu <email@example.com>
2025-04-10 15:43:13 +0700

    First commit

---
+++ woco-bab-5-main/src/screens/index.jsx
@@ -0,0 +1,9 @@
+import Home from "./Home";
+import BlogDetail from "./BlogDetail";
+import Discover from "./Discover";
+import Bookmark from "./Bookmark";
+import Profile from "./Profile";
+import AddBlogForm from "./AddBlogForm";
+
+export {Home, BlogDetail, Discover, Bookmark, Profile, AddBlogForm}
+
